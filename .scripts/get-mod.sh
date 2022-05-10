06e7d0e16a6fc3512f5760ba8a75546c2aae00fa
hexlo <24765567+hexlo@users.noreply.github.com>
2022-05-10 19:39:00 -0400

    build now properly download mods

--- .scripts/get-mod.sh
+++ .scripts/get-mod.sh
@@ -10,7 +10,7 @@ latestReleaseURL="${latestReleaseURL}/releases/latest"
 
 [[ -z ${2} ]] && dest=. || dest=${2%/}
 
-latestVersion=$(./get-mod-version.sh ${latestReleaseURL})
+latestVersion=$(get-mod-version.sh ${latestReleaseURL})
 # latestVersion=$(curl -L --silent \
 #     -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36" \
 #     ${latestReleaseURL} \
