c8bd9f7f6a4f6bb4c0c247bbf32e05f1af934709
hexlo <24765567+hexlo@users.noreply.github.com>
2022-09-08 15:44:49 -0400

    Changed tmodloader version to a static 0.11.8.9

--- .scripts/get-tmodloader-1_3.sh
+++ .scripts/get-tmodloader-1_3.sh
@@ -9,11 +9,15 @@ url='https://github.com/tModLoader/tModLoader/releases'
 latestVersion=$(curl -L --silent ${url} | grep "/tModLoader/tModLoader/releases/tag/v0." | head -1 | sed 's#^.*/tag/v##' | sed 's#".*$##')
 
 
-downloadURL=${url}/download/v${latestVersion}/tModLoader.Linux.v${latestVersion}.zip
+### Since tmodloader 1.3 is not receiving updates anymore, changing the url and echo output to static ones (0.11.8.9).
+
+# downloadURL=${url}/download/v${latestVersion}/tModLoader.Linux.v${latestVersion}.zip
+downloadURL='https://github.com/tModLoader/tModLoader/releases/download/v0.11.8.9/tModLoader.Linux.v0.11.8.9.zip'
 curl -L --silent ${downloadURL} --output ${dest}/tmodloader-server.zip
 
-echo "tModLoader: ${latestVersion}"
+# echo "tModLoader: ${latestVersion}"
+echo "tModLoader: 0.11.8.9"
+
 
 # curl -L https://github.com/tModLoader/tModLoader/releases | grep "/tModLoader/tModLoader/releases/tag/v0." | head -1 | sed 's#^.*/tag/v##' | sed 's#".*$##'
-
 # https://github.com/tModLoader/tModLoader/releases/download/v0.11.8.8/tmodLoader.tmod
